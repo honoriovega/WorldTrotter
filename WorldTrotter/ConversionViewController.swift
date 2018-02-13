4f3f3593b279405e80a2ac37a7369442d91498e8
Honorio Vega <hvega@csumb.edu>
2018-02-12 23:36:14 -0800

    Implementing the Temperature Conversion

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -10,22 +10,45 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     
+    let numberFormatter: NumberFormatter = {
+       let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
     
     @IBOutlet weak var celsiusLabel: UILabel!
     
     @IBOutlet var textField: UITextField!
+    var fahrenheitValue : Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+   
+    var celsiusValue : Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
 
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-        if let text = textField.text, !text.isEmpty {
-            celsiusLabel.text = text
+        
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
         } else {
-            celsiusLabel.text = "???"
-        }    }
+            fahrenheitValue = nil
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateCelsiusLabel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,6 +58,16 @@ class ConversionViewController: UIViewController {
     
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         textField.resignFirstResponder()
+    }
+    
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text =
+                numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 
 }
